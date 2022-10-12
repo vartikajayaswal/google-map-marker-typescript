a983962a14752611047d5e3709931276256e8c02
Vartika Jayaswal <vartikajayaswal@yahoo.com>
2022-10-12 13:21:34 +0530

    Initial Commit

---
+++ CustomMap.ts
@@ -0,0 +1,46 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+export interface Mappable{
+ location: {
+  lat : number,
+  lng : number
+ };
+ markerContent(): string;
+}
+
+
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+  
+
+  constructor (divId: string) {
+    const id = document.getElementById(divId);
+    this.googleMap = new google.maps.Map(id,{
+      zoom: 2,
+      center: {
+        lat:0,
+        lng:0
+      }
+    });
+  }
+
+  addMarker(mappable : Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
+
+  }
+}
